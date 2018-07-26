847b0b89273f175eba2b4eb380bbf48f604b77cb
Ahmer Malik <ahmer9800@gmail.com>
2018-07-26 15:27:50 -0500

    complete user.js model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+export default (sequelize, DataTypes) => {
+    const User = sequelize.define("user", {
+        username:{
+            type: DataTypes.STRING,
+            unique: true,
+        },
+        wallet_address:{
+            type: DataTypes.STRING,
+            unique: false,
+        },
+    });
+
+    User.associate = (models) => {
+       User.belongsToMany(models.Wallet, {
+            through:'wallet',
+            foreignKey: 'userId',
+        });
+    };
+
+    return User;
+};
