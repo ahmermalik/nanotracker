9000416403c9562d206a94c5407df25dd4446d46
Ahmer Malik <ahmer9800@gmail.com>
2018-08-04 22:16:56 -0500

    update models

---
+++ migrations/20180805031045-create-wallet.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('wallets', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      wallet_address: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('wallets');
+  }
+};
--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
     f_name: DataTypes.STRING,
     l_name: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {});
   user.associate = function(models) {
     // associations can be defined here
